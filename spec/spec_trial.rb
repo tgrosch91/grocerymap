1e1ff78d35663239f3131fa968d238d10856607f
Teresa Grosch <tgrosch91@gmail.com>
2016-11-18 22:43:48 +0000

    added test to check ability to accept url

--- spec/spec_trial.rb
+++ spec/spec_trial.rb
@@ -7,7 +7,8 @@ describe "Scraper" do
 
   describe "#get_page" do
     it "uses Nokogiri to get the HTML from a web page" do
-      doc = scraper.get_page
+      url = https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25
+      doc = scraper.get_page(url)
       expect{scraper.get_page}.to_not raise_error
       expect(doc).to be_a(Nokogiri::HTML::Document)
     end
@@ -16,7 +17,8 @@ describe "Scraper" do
 
   describe "#get_picture_links" do
     it "uses a CSS selector to return an array of Nokogiri XML elements representing the html links I will be using to get to individual kitten profiles" do
-      picture_links = scraper.get_picture_links
+      url = https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25
+      picture_links = scraper.get_picture_links(url)
       picture_links.each do |picture_link|
         expect(picture_link).to be_a(String)
         expect(picture_link).to include?("petdetail")
