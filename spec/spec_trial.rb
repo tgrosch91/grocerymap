e9b93a0d35c8537daac69bd52828263450c390e5
Teresa Grosch <tgrosch91@gmail.com>
2016-11-19 21:19:29 +0000

    forgot to change test method name

--- spec/spec_trial.rb
+++ spec/spec_trial.rb
@@ -5,10 +5,10 @@ describe "Scraper" do
 
   let(:scraper) {Scraper.new}
 
-  describe "#get_picture_links" do
+  describe "#get_individual_links" do
     it "uses a CSS selector to return an array of Nokogiri XML elements representing the html links I will be using to get to individual kitten profiles" do
       url = "http://theshelterpetproject.org/search?location=63109&animal=cat&breed=&sex=&size=&age=&offset=0&count=16&page=1"
-      links = scraper.get_picture_links(url)
+      links = scraper.get_individual_links(url)
       links.each do |link|
         expect(link).to be_a(String)
         expect(link).to include?("pet?id")
