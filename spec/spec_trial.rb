54616a76637e97a526bc1ac91f93695439afbdcd
Teresa Grosch <tgrosch91@gmail.com>
2016-11-19 16:53:24 +0000

    fixed small error

--- spec/spec_trial.rb
+++ spec/spec_trial.rb
@@ -7,7 +7,7 @@ describe "Scraper" do
 
   describe "#get_page" do
     it "uses Nokogiri to get the HTML from a web page" do
-      url = https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25
+      url = "https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25"
       doc = scraper.get_page(url)
       expect{scraper.get_page}.to_not raise_error
       expect(doc).to be_a(Nokogiri::HTML::Document)
@@ -17,7 +17,7 @@ describe "Scraper" do
 
   describe "#get_picture_links" do
     it "uses a CSS selector to return an array of Nokogiri XML elements representing the html links I will be using to get to individual kitten profiles" do
-      url = https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25
+      url = "https://www.petfinder.com/pet-search?location=63109&animal=cat&breed=&age=baby&distance=25"
       picture_links = scraper.get_picture_links(url)
       picture_links.each do |picture_link|
         expect(picture_link).to be_a(String)
