c360d9e98e70b2e837c21aa359cb3d53478adf1d
Teresa Grosch <tgrosch91@gmail.com>
2016-11-19 22:33:21 +0000

    located css selectors for the inidividual kitten info

--- lib/individual_link_scraper.rb
+++ lib/individual_link_scraper.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 class Scraper
 
 
-	def get_individual_links(url)
+	def get_picture_links(url)
 		doc = Nokogiri::HTML(open(url))
 		pet_urls = []
     	doc.css("#search_container .thumb").each do |pet|
@@ -15,4 +15,21 @@ class Scraper
     	pet_urls
 	end
 
+	def get_kitten_info(url)
+		doc = Nokogiri::HTML(open(url))
+		hash = {}
+		binding.pry
+	end
+
 end
+
+scraper= Scraper.new
+scraper.get_kitten_info("http://theshelterpetproject.org/pet?id=16655758")
+
+
+# name = doc.css(".sub-page-header a").text
+# breed = doc.css(".pet_details_left .breed").text.split(/:\s/)[1]
+# gender = doc.css(".pet_details_left .gender").text.split(/:\s/)[1]
+# age = doc.css(".pet_details_right .age").text.split(/:\s/)[1]
+# shelter = doc.css(".locale li:nth-child(2)").text.split(/:\s"|"/)[1]
+# bio = doc.css(".description p").text
