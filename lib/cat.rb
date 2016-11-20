874208709c808a6e2bb299e22b9d90163d2a0b10
Teresa Grosch <tgrosch91@gmail.com>
2016-11-20 04:08:36 +0000

    added gender class, began trying to figure out object relationships

--- lib/cat.rb
+++ lib/cat.rb
@@ -7,20 +7,35 @@ class Cat
 
 	@@all = []
 
-
-
-	def initialize(attributes)
-		attributes.each{|key,value| self.send(("#{key}="), value)}
-		@@all << self
+	def initialize(name)
+		@name = name
 	end
 
 	def self.all
 		@@all
 	end
 
-  def self.destroy_all
-    @@all.clear
-  end
+	def self.destroy_all
+	    @@all.clear
+	end
+
+	def create_by_hash(attributes)
+		attributes.each{|key,value| self.send(("#{key}="), value)}
+		@@all << self)
+	end
+
+	def cat_gender=(name)
+		self.gender = Gender.find_or_create_by_name(name)
+		gender.add_cat(self)
+	end
+
 
 
 end
+
+scraper = Scraper.new
+cat_hash = scraper.get_kitten_info("http://theshelterpetproject.org/pet?id=16655758")
+
+cat= Cat.new(cat_hash)
+
+print Cat.all
