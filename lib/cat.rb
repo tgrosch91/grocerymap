98f78912ab3f5853dd3215232c9e8b3b3ddabaa5
Teresa Grosch <tgrosch91@gmail.com>
2016-11-20 00:49:12 +0000

    created cat class

---
+++ lib/cat.rb
@@ -0,0 +1,31 @@
+require 'pry'
+require_relative '../lib/scraper'
+
+
+class Cat
+
+	attr_accessor :name, :breed, :gender, :age, :shelter, :bio
+
+	@@all = []
+
+
+
+	def initialize(attributes)
+		attributes.each{|key,value| self.send(("#{key}="), value)}
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+
+end
+
+cat_scraper = Scraper.new
+
+cat_hash = cat_scraper.get_kitten_info("http://theshelterpetproject.org/pet?id=16655758")
+
+new_cat= Cat.new(cat_hash)
+
+print Cat.all
