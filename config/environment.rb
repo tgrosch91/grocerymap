435d5555183248c5e45a2e63e0568ab859d0c355
Teresa Grosch <tgrosch91@gmail.com>
2016-11-20 00:13:52 +0000

    Set up gemfile and environment, files for cat class and spec

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'bundler'
+Bundler.require
+
+require 'pry'
+
+
+require_relative '../lib/cat.rb'
+require_relative '../lib/scraper.rb'
+
+
+require_all 'lib'
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,5 +1,5 @@
 require_relative './spec_helper'
-require_relative '../lib/individual_link_scraper.rb'
+require_relative '../lib/scraper.rb'
 
 describe "Scraper" do
 
